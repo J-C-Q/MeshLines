ca86c0edc8532a171b3aa0e5f7ee55eb697d1fc0
Quinten Preiß <preissquinten@gmail.com>
2025-02-20 21:48:30 +0100

    working

--- src/MeshLine.jl
+++ src/MeshLine.jl
@@ -1,6 +1,7 @@
 module MeshLine
 using LinearAlgebra
 using Rotations
+using GeometryBasics
 using MakieCore
 
 include("recipes.jl")
--- src/recipes.jl
+++ src/recipes.jl
@@ -1,6 +1,6 @@
 @MakieCore.recipe(MeshLines, points) do scene
     MakieCore.Attributes(
-        around = 3,
+        around=10,
         between = 0,
         width = 0.1
     )
@@ -52,98 +52,140 @@ function intersectPlaneNotZeroAndLinear(x::MakieCore.Vec3f,y::MakieCore.Vec3f)
     return zvec,normalvec
 end
 
-function sin_angle_between_vectors(a, b)
-    return norm(cross(a, b)) / (norm(a) * norm(b))
+function cos_angle_between_vectors(a, b)
+    return abs(dot(a, b)) / (norm(a) * norm(b))
 end
 
 function r_ellipse(ϕ;a=1,b=1)
     return a*b/sqrt(a^2*sin(ϕ)^2+b^2*cos(ϕ)^2)
 end
 
-
-function MakieCore.plot!(ml::MeshLines{<:Tuple{<:AbstractVector{<:MakieCore.Point3f}}})
-    linespoints = ml[1][]
-    n_around = ml.around[]
-    pointsaround = Matrix{MakieCore.Point3f}(undef,n_around,length(linespoints))
-    angle = 2π/n_around
-
-
-    #  beginning and end are not elliptic
-    vector = MakieCore.Vec3f(linespoints[2] .- linespoints[1])
-    normalvec = normal(vector)
-    R = AngleAxis(angle, vector...)
-    for j in 1:n_around
-        pointsaround[j,1] = R^(j-1)*normalvec.*ml.width[] .+ linespoints[1]
-    end
-    vector = MakieCore.Vec3f(linespoints[end] .- linespoints[end-1])
-    normalvec = normal(vector)
-    R = AngleAxis(angle, vector...)
-    for j in 1:n_around
-        pointsaround[j,end] = R^(j-1)*normalvec.*ml.width[] .+ linespoints[end]
-    end
-
-
-
-    for i in 2:length(linespoints)-1
-        p = linespoints[i]
-        prevPoint = linespoints[i-1]
-        nextPoint = linespoints[i+1]
-
-        v1 = MakieCore.Vec3f(p .- prevPoint)
-        v2 = MakieCore.Vec3f(nextPoint .- p)
-
-        # linearly dependent
-        if rank(hcat(v1,v2)) == 1
-            normalvec = normal(v1)
-            R = AngleAxis(angle, v1...)
-            for j in 1:n_around
-                pointsaround[j,1] = R^(j-1)*normalvec.*ml.width[] .+ p
-            end
-        else
-            vec,normalvec = intersectPlaneNotZeroAndLinear(v1,v2)
-            R = AngleAxis(angle, vec...)
-            sinang = sin_angle_between_vectors(v1, vec)
-            println(sinang)
-            for j in 1:n_around
-                pointsaround[j,i] = R^(j-1)*normalvec.*r_ellipse(angle*(j-1);b=ml.width[],a=ml.width[]/sinang) .+ p
-            end
+function align_ring(prev_ring::Vector{MakieCore.Point3f}, cur_ring::Vector{MakieCore.Point3f})
+    n = length(prev_ring)
+    best_shift = 0
+    best_sum = Inf
+    # Try every possible circular shift
+    for shift in 0:(n-1)
+        # Use circular shift to reorder cur_ring
+        shifted_ring = circshift(cur_ring, shift)
+        total_dist = sum(norm(prev_ring[i] - shifted_ring[i]) for i in 1:n)
+        if total_dist < best_sum
+            best_sum = total_dist
+            best_shift = shift
         end
     end
+    return circshift(cur_ring, best_shift)
+end
 
-    display(pointsaround)
-    pointsaroundVector = vec(reshape(pointsaround,(length(pointsaround),1)))
-    vertices = Matrix{Float32}(undef,length(pointsaround),3)
-    for (i,p) in enumerate(pointsaroundVector)
-        vertices[i,:] .= p
-    end
-    # faces = Matrix{Int32}(undef,length(linespoints)*(n_around-1),3)
-    faces = Matrix{Int32}(undef,4n_around,3)
-    for j in 1:n_around
-        faces[j,1] = j
-        faces[j,2] = mod1(j+1,n_around)
-        faces[j,3] = mod1(j+1,n_around)+n_around
-    end
-    # for i in 1:length(linespoints)-1
-    for j in 1:n_around
-        faces[j+n_around,1] = j + n_around
-        faces[j+n_around,2] = mod1(j+1,n_around) + n_around
-        faces[j+n_around,3] = mod1(j,n_around)
+function MakieCore.plot!(ml::MeshLines{<:Tuple{<:AbstractVector{<:MakieCore.Point3f}}})
 
-        faces[j+2n_around,1] = j + n_around
-        faces[j+2n_around,2] = mod1(j+1,n_around) + n_around
-        faces[j+2n_around,3] = mod1(j,n_around) + 2n_around
-    end
-    for j in 1:n_around
-        faces[j+3n_around,1] = j+ 2n_around
-        faces[j+3n_around,2] = mod1(j+1,n_around) + 2n_around
-        faces[j+3n_around,3] = mod1(j+1,n_around)+n_around
+    points = ml[1]
+    around = ml.around
+
+    vertices_ob = MakieCore.Observable(Matrix{Float32}(undef, length(points[]) * around[], 3))
+    faces_ob = MakieCore.Observable(Matrix{UInt32}(undef, 2 * around[] * (length(points[]) - 1), 3))
+
+
+
+
+
+    function update_plot(points)
+        vertices = Matrix{Float32}(undef, length(points) * around[], 3)
+        faces = zeros(UInt32, 2 * around[] * (length(points) - 1), 3)
+
+        linespoints = points
+        n_around = around[]
+
+        pointsaround = Matrix{MakieCore.Point3f}(undef, n_around, length(linespoints))
+        angle = 2π / n_around
+
+
+        #  beginning and end are not elliptic
+        vector = MakieCore.Vec3f(linespoints[2] .- linespoints[1])
+        normalvec = normal(vector)
+        R = AngleAxis(angle, vector...)
+        for j in 1:n_around
+            pointsaround[j, 1] = R^(j - 1) * normalvec .* ml.width[] .+ linespoints[1]
+        end
+        vector = MakieCore.Vec3f(linespoints[end] .- linespoints[end-1])
+        normalvec = normal(vector)
+        R = AngleAxis(angle, vector...)
+        for j in 1:n_around
+            pointsaround[j, end] = R^(j - 1) * normalvec .* ml.width[] .+ linespoints[end]
+        end
+
+
+
+        for i in 2:length(linespoints)-1
+            p = linespoints[i]
+            prevPoint = linespoints[i-1]
+            nextPoint = linespoints[i+1]
+
+            v1 = MakieCore.Vec3f(p .- prevPoint)
+            v2 = MakieCore.Vec3f(nextPoint .- p)
+
+            # linearly dependent
+            if rank(hcat(v1, v2)) == 1
+                normalvec = normal(v1)
+                R = AngleAxis(angle, v1...)
+                for j in 1:n_around
+                    pointsaround[j, 1] = R^(j - 1) * normalvec .* ml.width[] .+ p
+                end
+            else
+                #elliptic
+                vec, normalvec = intersectPlaneNotZeroAndLinear(v1, v2)
+                R = AngleAxis(angle, vec...)
+                cosang = cos_angle_between_vectors(v1, vec)
+                for j in 1:n_around
+                    pointsaround[j, i] = R^(j - 1) * normalvec .* r_ellipse(angle * (j - 1); b=ml.width[], a=ml.width[] / cosang) .+ p
+                end
+            end
+        end
+
+        pointsaroundVector = vec(reshape(pointsaround, (length(pointsaround), 1)))
+
+        rings = [[pointsaround[j, i] for j in 1:n_around] for i in 1:length(linespoints)]
+        # Align each ring (starting from the second) with the previous one:
+        for i in 2:length(rings)
+            rings[i] = align_ring(rings[i-1], rings[i])
+        end
+
+        # Flatten the rings into your vertices array (ensure the same order is maintained):
+        vertices = vertices_ob[]
+        for (i, ring) in enumerate(rings)
+            for j in 1:n_around
+                idx = (i - 1) * n_around + j
+                vertices[idx, :] .= ring[j]
+            end
+        end
+
+        num_rings = length(linespoints)
+        faces = faces_ob[]
+        face_idx = 1
+        for i in 1:(num_rings-1)
+            for j in 1:n_around
+                next_j = mod1(j + 1, n_around)
+                # First triangle of the quad
+                faces[face_idx, :] = [(i - 1) * n_around + j,
+                    (i - 1) * n_around + next_j,
+                    i * n_around + j]
+                face_idx += 1
+                # Second triangle of the quad
+                faces[face_idx, :] = [(i - 1) * n_around + next_j,
+                    i * n_around + next_j,
+                    i * n_around + j]
+                face_idx += 1
+            end
+        end
+        vertices_ob[] = vertices
+        faces_ob[] = faces
     end
 
-    mesh!(ml, vertices, faces; transparency=false, overdraw=false)
+    MakieCore.Observables.onany(update_plot, points)
+    update_plot(points[])
 
-    display(vertices)
-    meshscatter!(ml,vec(reshape(pointsaround,(length(pointsaround),1))), markersize = 0.01)
-    meshscatter!(ml,vec(pointsaround[1,:]), markersize = 0.01,color=:red)
-    lines!(ml,ml[1])
+    mesh!(ml, vertices_ob, faces_ob; transparency=false, overdraw=false, color=:red)
+
+
     ml
 end
